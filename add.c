a64e8775de5661fe11f02e785cf53f0f9cca3c52
ponraj73 <ponraj5656@gmail.com>
2026-05-17 21:35:44 +0530

    update the add file

--- add.c
+++ add.c
@@ -5,14 +5,31 @@ int add(int arg1 , int arg2){
     return arg1 + arg2;
 }
 
+/* Important task */
+int calculate(int a, int b, int func){
+    switch(func){
+        case 1:
+            add(a, b);
+            break;
+        case 2:
+            return a - b;
+            break;
+        default:
+            printf("\r\n Enter a valid operation\r\n");
+    }
+}
+
 int main(){
 
-    int var1,var2,sum;
+    int var1,var2,sum, diff;
     var1 = 10;
     var2 = 20;
 
-    add(var1, var2);
+    sum = calculate(var1, var2, 1);
+    diff = calculate(var1, var2, 2);
 
     printf("\r\n the sum of the two numbers %d adn %d is %d \r\n", var1, var2, sum);
+    printf("\r\n The differnce between %d and %d is %d \r\n", var1, var2, diff);
+    
     return 0;
 }
